837da62da1c379a431e1403a851d8a0fba68277a
Carter Rogers <carter.d.rogers@gmail.com>
2025-05-22 02:41:23 -0500

    Create TagController & GET /Tags, Issue #5 & Issue #3

---
+++ LeaflineApi/Controllers/TagController.cs
@@ -0,0 +1,30 @@
+﻿using LeaflineApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaflineApi.Controllers
+{
+
+  [ApiController]
+  [Route("Tags")]
+  public class TagController : ControllerBase
+  {
+
+    private readonly ILogger<TagController> _logger;
+    private readonly ApiContext _context;
+
+    public TagController(ILogger<TagController> logger, ApiContext context)
+    {
+      _logger = logger;
+      _context = context;
+    }
+
+    [HttpGet]
+    public async Task<List<LeaflineTag>> ListTags() {
+      var tags = await _context.tags.ToListAsync();
+      return tags;
+    }
+
+
+  }
+}

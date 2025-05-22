abf8bc53c04cf1d8f1879411e9968d90a648df13
Carter Rogers <carter.d.rogers@gmail.com>
2025-05-22 02:49:22 -0500

    Add POST /CreateTag, Issue #5

--- LeaflineApi/Controllers/TagController.cs
+++ LeaflineApi/Controllers/TagController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 namespace LeaflineApi.Controllers
 {
@@ -22,9 +23,39 @@ namespace LeaflineApi.Controllers
     }
 
     [HttpGet]
-    public async Task<List<LeaflineTag>> ListTags() {
+    public async Task<List<LeaflineTag>> ListTags()
+    {
       var tags = await _context.tags.ToListAsync();
       return tags;
+    }
+
+    [HttpPost]
+    [Route("/Tags/CreateTag")]
+    public async Task<LeaflineResponse> CreateTag(string tag)
+    {
+      LeaflineResponse response = new LeaflineResponse();
+      var existingTag = await _context.tags.FirstOrDefaultAsync(t => t.Tag.ToLower().Equals(tag));
+      if(existingTag == null) {
+        var newTag = new LeaflineTag
+        {
+          TagId = Guid.NewGuid(),
+          Tag = tag
+        };
+
+        var result = await _context.tags.AddAsync(newTag);
+        await _context.SaveChangesAsync();
+
+        response.isSuccess = true;
+        response.Message = "Tag created successfully!";
+        response.Content = JsonSerializer.Serialize(result);
+      }else {
+        response.isSuccess = false;
+        response.Message = "Tag already exists!";
+        response.Content = null;
+      }
+
+
+      return response;
     }
 
   }
